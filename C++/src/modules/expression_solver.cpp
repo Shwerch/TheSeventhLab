6dabc9b7e41104f58a064d6a9c0479b2ed16b33a
Shwerch <ilia06popov@gmail.com>
2025-09-17 10:24:55 +0700

    Work in progress

--- C++/src/modules/expression_solver.cpp
+++ C++/src/modules/expression_solver.cpp
@@ -2,38 +2,33 @@
 
 #include <iostream>
 
-bool dfs_all(const std::vector<int> &nums, size_t index, int current_value, int last_term, int target,
-			 std::string expr) {
-	if (index == nums.size()) {
-		if (current_value == target) {
-			std::cout << expr << " = " << target << std::endl;
+bool calculate(const std::vector<int> &numbers, size_t index, int summa,
+			   int last_term, int S, std::string expr) {
+	if (index == numbers.size()) {
+		if (summa == S) {
+			std::cout << expr << " = " << S << std::endl;
 			return true;
 		}
 		return false;
 	}
 
-	int next = nums[index];
+	int next = numbers.at(index);
 
-	// Пробуем сложение: новое слагаемое начинается
-	if (dfs_all(nums, index + 1,
-				current_value + next, // добавляем в сумму
-				next,				  // новое последнее слагаемое
-				target, expr + " + " + std::to_string(next)))
+	if (calculate(numbers, index + 1, summa + next, next, S,
+				  expr + " + " + std::to_string(next)))
 		return true;
 
-	// Пробуем умножение: корректируем последнее слагаемое
-	if (dfs_all(nums, index + 1,
-				current_value - last_term + last_term * next, // пересчитываем сумму с учётом умножения
-				last_term * next,							  // новое последнее слагаемое
-				target, expr + " * " + std::to_string(next)))
+	if (calculate(numbers, index + 1, summa - last_term + last_term * next,
+				  last_term * next, S, expr + " * " + std::to_string(next)))
 		return true;
 
 	return false;
 }
 
-bool expressionCanFormTarget(const std::vector<int> &numbers, int target) {
+bool expressionCanFormTarget(const std::vector<int> &numbers, int S) {
 	if (numbers.empty())
 		return false;
 
-	return dfs_all(numbers, 1, numbers[0], numbers[0], target, std::to_string(numbers[0]));
+	return calculate(numbers, 1, numbers[0], numbers[0], S,
+					 std::to_string(numbers[0]));
 }
